78d9365645e201812d22649068fb1b0104792fa4
Dhimas Pandu Yogaswara <79622243+Dhimasswara@users.noreply.github.com>
2023-04-17 17:37:41 +0700

    update query.sql

--- query.sql
+++ query.sql
@@ -9,7 +9,7 @@ create table workers (
     phone varchar(255) not null,
     password varchar(255) not null,
     jobdesk varchar(255),
-    description varchar(255),
+    description text,
     address varchar(255),
     workplace varchar(255),
     role varchar(255)
@@ -18,31 +18,45 @@ create table workers (
 
 create table skills (
     id_skill varchar(255) primary key,
-    name varchar(255) not null,
+    name_skill varchar(255) not null,
     id_worker varchar(255),
-    CONSTRAINT fk_worker FOREIGN KEY (id_worker) REFERENCES workers(id_worker)
+    CONSTRAINT fk_worker FOREIGN KEY (id_worker) REFERENCES workers(id_worker) ON UPDATE CASCADE ON DELETE CASCADE
 )
 
 create table portfolio (
     id_portfolio varchar(255) primary key,
-    name varchar(255) not null,
-    repository varchar(255),
-    type varchar(255),
+    name_portfolio varchar(255) not null,
+    image varchar(255),
+    repo_link varchar(255),
+    type_portfolio varchar(255),
     id_worker varchar(255),
-    CONSTRAINT fk_worker FOREIGN KEY (id_worker) REFERENCES workers(id_worker)
+    CONSTRAINT fk_worker FOREIGN KEY (id_worker) REFERENCES workers(id_worker) ON UPDATE CASCADE ON DELETE CASCADE
 )
+
+create table experiences (
+    id_experience varchar(255) primary key,
+    id_worker varchar(255),
+    jobdesk varchar(255),
+    company_name varchar(80),
+    date_start varchar(20),
+    date_end varchar(20),
+    description text,
+    image varchar(255),
+    CONSTRAINT fk_worker FOREIGN KEY (id_worker) REFERENCES workers(id_worker) ON UPDATE CASCADE ON DELETE CASCADE
+)    
+    
 
 
 
 create table recruiters (
     id_recruiter varchar(255) primary key,
-    fullname varchar(255) not null,
+    name varchar(255) not null,
     email varchar(255) not null,
     company_name varchar(255) ,
     position varchar(255),
     job_field varchar(255),
     city varchar(255),
-    description_company varchar(255),
+    description_company text,
     instagram varchar(255),
     phone varchar(255) not null,
     linkedin varchar(255),
